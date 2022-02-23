0edbfb98ca81b30d8440b94e7a94c0f2cfc440d8
Pierce Brooks <piercebrooks@rocketmail.com>
2022-02-23 18:55:02 -0500

    improve initial build experience

--- Build.bat
+++ Build.bat
@@ -1,2 +1,5 @@
+python -m pip install -r ./Buildster/requirements.txt
+python -m pip install -r ./requirements.txt
 git submodule update --init --recursive
 python ./Buildster/buildster.py ./Project.xml
+
--- Build.sh
+++ Build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+python3 -m pip install -r ./Buildster/requirements.txt
+python3 -m pip install -r ./requirements.txt
 git submodule update --init --recursive
 python3 ./Buildster/buildster.py ./Project.xml
 
