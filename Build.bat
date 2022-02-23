60c0ee11696ea3ae1c9baffc4d020fe05b669934
Pierce Brooks <piercebrooks@rocketmail.com>
2022-02-23 18:57:02 -0500

    reorder build steps

--- Build.bat
+++ Build.bat
@@ -1,5 +1,4 @@
-python -m pip install -r ./Buildster/requirements.txt
-python -m pip install -r ./requirements.txt
 git submodule update --init --recursive
+python -m pip install -r ./Buildster/requirements.txt
 python ./Buildster/buildster.py ./Project.xml
 
--- Build.sh
+++ Build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-python3 -m pip install -r ./Buildster/requirements.txt
-python3 -m pip install -r ./requirements.txt
 git submodule update --init --recursive
+python3 -m pip install -r ./Buildster/requirements.txt
 python3 ./Buildster/buildster.py ./Project.xml
 
